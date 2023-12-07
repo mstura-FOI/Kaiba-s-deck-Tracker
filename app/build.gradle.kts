33ccfebf590f9e2b21fbb1a85add733d4444f2f7
mstura <mstura21@student.foi.hr>
2023-12-07 18:02:19 +0100

    Added all data classes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.kaiba.tracker"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.kaiba.tracker"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/com/kaiba/tracker/data/Objects.kt
@@ -0,0 +1,33 @@
+package com.kaiba.tracker.data
+
+data class YuGiOhCard(
+    val id: Int,
+    val name: String,
+    val type: String,
+    val frameType: String,
+    val description: String,
+    val attack: Int,
+    val defense: Int,
+    val level: Int,
+    val race: String,
+    val attribute: String,
+    val archetype: String,
+    val ygoprodeckUrl: String,
+    val cardImages: List<CardImage>,
+    val cardPrices: List<CardPrice>
+)
+
+data class CardImage(
+    val id: Int,
+    val imageUrl: String,
+    val imageUrlSmall: String,
+    val imageUrlCropped: String
+)
+
+data class CardPrice(
+    val cardmarketPrice: String,
+    val tcgplayerPrice: String,
+    val ebayPrice: String,
+    val amazonPrice: String,
+    val coolstuffincPrice: String
+)
