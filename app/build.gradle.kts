ba3dbae1910cf5b9edc42c3062e3a93b737e50f3
mstura <mstura21@student.foi.hr>
2023-12-07 18:27:42 +0100

    Added Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation ("com.google.code.gson:gson:2.10.1")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
---
+++ app/src/main/java/com/kaiba/tracker/httpRequestManager/YugiService.kt
@@ -0,0 +1,12 @@
+package com.kaiba.tracker.httpRequestManager
+
+import com.kaiba.tracker.data.YuGiOhCard
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+interface YugiService {
+    @GET("api/v7/cardinfo.php")
+    suspend fun getCards() : Response<YuGiOhCard>
+}
+
