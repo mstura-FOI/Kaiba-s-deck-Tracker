fc6c4dfba114127c59230b84154e4a9e7a9a6628
mstura <mstura21@student.foi.hr>
2023-12-11 22:23:04 +0100

    Functional card added

--- app/src/main/java/com/kaiba/tracker/uiComponents/Card.kt
+++ app/src/main/java/com/kaiba/tracker/uiComponents/Card.kt
@@ -1,13 +1,18 @@
 package com.kaiba.tracker.uiComponents
 
 
+import android.icu.text.ListFormatter.Width
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,12 +25,15 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.kaiba.tracker.R
 import com.kaiba.tracker.data.YuGiOhCard
  var yuGiOhCard:YuGiOhCard? = null
@@ -43,7 +51,8 @@ fun CardInfo(
             containerColor = Color(0xff3d3e51),
         ),
         modifier = Modifier
-            .size(width = 240.dp, height = 100.dp)
+            .fillMaxWidth()
+            .height(120.dp)
 
     ) {
         Row(
@@ -67,10 +76,17 @@ fun CardInfo(
                )
            }
             Spacer(modifier = Modifier.padding(10.dp))
-            Image(painter = painterResource(id = R.drawable.img),
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(if(card == null) "" else "${card.card_images[0].image_url_cropped}")
+                    .crossfade(true)
+                    .build(),
+                placeholder = painterResource(R.drawable.img),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.padding(12.dp))
+                modifier = Modifier.padding(12.dp)
+            )
+
         }
 
 
