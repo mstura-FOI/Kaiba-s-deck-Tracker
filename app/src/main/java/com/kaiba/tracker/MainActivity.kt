04cfe9b7171a1c44086d0e9c09e8ab040b72e10e
mstura <mstura21@student.foi.hr>
2023-12-12 00:20:03 +0100

    Added progress circle for loading cards

--- app/src/main/java/com/kaiba/tracker/MainActivity.kt
+++ app/src/main/java/com/kaiba/tracker/MainActivity.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,8 +20,13 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kaiba.tracker.data.DataCard
@@ -30,10 +39,12 @@ import com.kaiba.tracker.uiComponents.ScrollableList
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.Dispatcher
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("CoroutineCreationDuringComposition")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -41,14 +52,35 @@ class MainActivity : ComponentActivity() {
             var cards by remember {
                 mutableStateOf<List<YuGiOhCard?>?>(null)
             }
+            var currentProgress by remember { mutableStateOf(0f) }
+            var loading by remember { mutableStateOf(true) }
+            val scope = rememberCoroutineScope()
 
-            LaunchedEffect(true) {
-
-                    cards = GetCard()
-                    Log.i("yugi", cards.toString())
-
+            scope.launch{
+                cards = GetCard()
+                loading = false
             }
-            ScrollableList(cards = cards)
+            if (loading){
+                Column(modifier = Modifier.fillMaxSize().padding(24.dp)
+                , verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.width(64.dp),
+                        color = Color.DarkGray,
+
+                        )
+                    Spacer(modifier = Modifier.padding(0.dp,24.dp))
+                    Text(text = stringResource(id = R.string.loading), fontWeight = FontWeight.Bold)
+            }
+
+            }else{
+
+                ScrollableList(cards = cards)
+            }
+
+
+
 
 
           }
